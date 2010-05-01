7f638966e6a99b630cfa240244066234690af2e3
DanielTichy <tichy.daniel@email.cz>
2010-05-01 20:47:45 +0200

    Final chcnges 201000501

--- templates/ja_raite/html/mod_footer/default.php
+++ templates/ja_raite/html/mod_footer/default.php
@@ -16,5 +16,5 @@
 
 */
 defined('_JEXEC') or die('Restricted access'); ?>
-<small><?php echo $lineone; ?> Designed by <a href="http://www.joomlart.com/" title="Visit Joomlart.com!" target="blank">JoomlArt.com</a>.</small>
+<small><?php echo $lineone; ?> Powered by Dwp Studio 69 & Joomla Framework gpl-2.0 <a href="http://www.joomlart.com/" title="Visit Joomlart.com!" target="blank">JoomlArt.com</a>.</small>
 <!--<small><?php echo JText::_( 'FOOTER_LINE2' ); ?></small> -->
--- templates/ja_raite/index.php
+++ templates/ja_raite/index.php
@@ -6,12 +6,12 @@
 # ------------------------------------------------------------------------
 # Copyright (C) 2004-2010 JoomlArt.com. All Rights Reserved.
 # @license - PHP files are GNU/GPL V2. CSS / JS are Copyrighted Commercial,
-# bound by Proprietary License of JoomlArt. For details on licensing, 
+# bound by Proprietary License of JoomlArt. For details on licensing,
 # Please Read Terms of Use at http://www.joomlart.com/terms_of_use.html.
 # Author: JoomlArt.com
 # Websites:  http://www.joomlart.com -  http://www.joomlancers.com
-# Redistribution, Modification or Re-licensing of this file in part of full, 
-# is bound by the License applied. 
+# Redistribution, Modification or Re-licensing of this file in part of full,
+# is bound by the License applied.
 # ------------------------------------------------------------------------
 
 */
@@ -85,12 +85,12 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 <!-- HEADER -->
 <div id="ja-header" class="wrap">
 <div class="main clearfix">
-  
-	<?php 
-	$siteName = $tmpTools->sitename(); 
+
+	<?php
+	$siteName = $tmpTools->sitename();
 	if ($tmpTools->getParam('logoType')=='image') { ?>
 	<h1 class="logo"><a href="index.php" title="<?php echo $siteName; ?>"><?php echo $siteName; ?></a></h1>
-	<?php } else { 
+	<?php } else {
 	$logoText = (trim($tmpTools->getParam('logoText'))=='') ? $config->sitename : $tmpTools->getParam('logoText');
 	$sloganText = (trim($tmpTools->getParam('sloganText'))=='') ? JText::_('SITE SLOGAN') : $tmpTools->getParam('sloganText');	?>
 	<div class="logo-text">
@@ -98,13 +98,13 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 		<p class="site-slogan"><?php echo $sloganText;?></p>
 	</div>
 	<?php } ?>
-	
+
 	<?php if($this->countModules('user4')) : ?>
 	<div id="ja-search">
 		<jdoc:include type="modules" name="user4" />
 	</div>
 	<?php endif; ?>
-	
+
 </div>
 </div>
 <!-- //HEADER -->
@@ -114,7 +114,7 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 ?>
 
 <!-- MAIN NAVIGATION -->
-<div id="ja-mainnav" class="wrap <?php if($slide) { echo ' hasslide'; } else { echo ' noslide'; } ?>">  	
+<div id="ja-mainnav" class="wrap <?php if($slide) { echo ' hasslide'; } else { echo ' noslide'; } ?>">
 <div class="main clearfix">
 	<?php if ($jamenu) $jamenu->genMenu (0); ?>
   	<ul class="no-display">
@@ -137,7 +137,7 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 <div id="ja-container<?php echo $divid; ?>" class="wrap">
 <div class="main clearfix">
 
-  	<!-- CONTENT -->  
+  	<!-- CONTENT -->
   	<div id="ja-content">
 
 		<jdoc:include type="message" />
@@ -153,13 +153,13 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 			  <jdoc:include type="modules" name="user1" style="xhtml" />
 			</div>
 			<?php } ?>
-			
+
 			<?php if ( $this->countModules('user2') ) { ?>
 			<div class="ja-box<?php echo $sl['user2']['class']; ?>" style="width: <?php echo $sl['user2']['width']; ?>;">
 			  <jdoc:include type="modules" name="user2" style="xhtml" />
 			</div>
 			<?php } ?>
-			
+
 			<?php if ( $this->countModules('user5') ) { ?>
 			<div class="ja-box<?php echo $sl['user5']['class']; ?>" style="width: <?php echo $sl['user5']['width']; ?>;">
 			  <jdoc:include type="modules" name="user5" style="xhtml" />
@@ -167,15 +167,15 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 			<?php } ?>
 		</div>
 		<?php } ?>
-		
+
 		<div id="ja-current-content" class="clearfix">
 			<jdoc:include type="component" />
 		</div>
 
 	</div>
   	<!-- //CONTENT -->
-	
-	 <!-- COLUMN --> 
+
+	 <!-- COLUMN -->
 	<?php if ( $ja_left || $ja_right || $hasSubnav ): ?>
 	<div id="ja-col">
 	<div class="ja-innerpad clearfix">
@@ -190,13 +190,13 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 	</div>
 	</div>
 	<?php endif; ?>
-	<!-- //COLUMN --> 
+	<!-- //COLUMN -->
 
 </div>
 </div>
 <div id="ja-pathway" class="wrap">
 	<div class="main clearfix">
-		<strong><?php echo JText::_('YOU ARE HERE:');?></strong> <jdoc:include type="module" name="breadcrumbs" />
+		<strong>&gt;</strong> <jdoc:include type="module" name="breadcrumbs" />
 	</div>
 </div>
 
@@ -213,13 +213,13 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 	  <jdoc:include type="modules" name="user6" style="xhtml" />
 	</div>
 	<?php } ?>
-	
+
 	<?php if ( $this->countModules('user7') ) { ?>
 	<div class="ja-box<?php echo $sl['user7']['class']; ?>" style="width: <?php echo $sl['user7']['width']; ?>;">
 	  <jdoc:include type="modules" name="user7" style="xhtml" />
 	</div>
 	<?php } ?>
-	
+
 	<?php if ( $this->countModules('user8') ) { ?>
 	<div class="ja-box<?php echo $sl['user8']['class']; ?>" style="width: <?php echo $sl['user8']['width']; ?>;">
 	  <jdoc:include type="modules" name="user8" style="xhtml" />
@@ -233,8 +233,11 @@ include_once (dirname(__FILE__).DS.'ja_vars_1.5.php');
 <!-- FOOTER -->
 <div id="ja-footer" class="wrap">
 <div class="main">
-	<jdoc:include type="modules" name="user3" />
-	<jdoc:include type="modules" name="footer" />
+ <span>Copyright &#169; 2010 Kraj Vyso&#269;ina | RMEDIA. V&#353;echna pr&#225;va vyhrazena. Web-support<a href="http://dwpstudio.ic.cz/"
+title="Kreativn&#237; my&#353;len&#237; pro V&#225;&#353; Web umo&#382;n&#237; dobr&#253; Graficky zpracovan&#253; layout. V&#225;&#353; prohl&#237;&#382;e&#269; si n&#225;&#353; Design zamiluje. Na&#353;e str&#225;nky jsou Vid&#283;t. Bu&#271;t&#283; vid&#283;t na internetu.
+I internetov&#233; str&#225;nky mohou b&#253;t um&#283;leck&#233; d&#237;lo, kter&#233; si obl&#237;b&#237;te spole&#269;n&#283; s obchodn&#237;mi partnery, se kter&#253;mi pr&#225;v&#283; zde,  m&#367;&#382;ete b&#253;t za kr&#225;tko p&#345;&#225;teli.
+Internetov&#233; &#345;e&#353;en&#237;, profesion&#225;ln&#237; design na m&#237;ru ka&#382;d&#233;mu. On-line store ve kter&#233;m se prodej Va&#353;ich produkt&#367; stane hra&#269;kou. Corporate identity. Because we'can. Worth'it - Trust'it.
+Copyright © 2009 :: E :: D :: F :: W :: J :: P :: M :: Q ::. V&#353;echna pr&#225;va vyhrazena. Designed by JoomlArt.com." target="blank">Dwp.Studio</a></span>
 </div>
 </div>
 <!-- //FOOTER -->
